dea077dfa7df9eefae7125d75eb74e6ed980db67
Jesse  Tochukwu <71229278+HeDevedUp@users.noreply.github.com>
2023-02-04 14:15:52 +0000

    feat: added new feature

--- src/index.js
+++ src/index.js
@@ -3,22 +3,3 @@ import Inputhandle from './modules/InputHandler.js';
 
 Inputhandle();
 
-// Toggle dark/light mode
-const sunIcon = document.querySelector('.sun');
-const moonIcon = document.querySelector('.moon');
-
-const darkMode = () => {
-  document.querySelector('html').style.filter = 'invert(0) hue-rotate(0deg)';
-  sunIcon.style.display = 'none';
-  moonIcon.style.display = 'block';
-};
-
-const lightMode = () => {
-  document.querySelector('html').style.filter = 'invert(0) hue-rotate(0deg)';
-  document.querySelector('html').style.filter = 'invert(1) hue-rotate(180deg)';
-  sunIcon.style.display = 'block';
-  moonIcon.style.display = 'none';
-};
-
-sunIcon.addEventListener('click', darkMode);
-moonIcon.addEventListener('click', lightMode);
